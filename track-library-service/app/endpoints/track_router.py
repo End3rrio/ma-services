395c6df5a7aeaa8d5165e9128d1031c4d7b754c2
End3rrio <mikhienkov@inbox.ru>
2024-02-21 13:24:54 +0300

    Initial observability

--- track-library-service/app/endpoints/track_router.py
+++ track-library-service/app/endpoints/track_router.py
@@ -1,17 +1,51 @@
 import uuid
 from uuid import UUID
-from fastapi import APIRouter, Depends, HTTPException, Body
+from fastapi import APIRouter, Depends, HTTPException, Body, Response
 
 from app.models.track import Track, CreateTrackRequest
 from app.services.music_catalog_service import TrackCatalogService
 
+import prometheus_client
+
 track_catalog_router = APIRouter(prefix='/track-catalog', tags=['TrackCatalog'])
+
+metrics_router = APIRouter(tags=['Metrics'])
+
+get_tracks_count = prometheus_client.Counter(
+    "get_tracks_count",
+    "Number of get requests for tracks"
+)
+
+get_track_count = prometheus_client.Counter(
+    "get_track_count",
+    "Number of get requests for track by id"
+)
+
+add_track_count = prometheus_client.Counter(
+    "add_track_count",
+    "Number of created tracks"
+)
+update_track_count = prometheus_client.Counter(
+    "update_track_count",
+    "Number of updated tracks"
+)
+
+delete_track_count = prometheus_client.Counter(
+    "delete_track_count",
+    "Number of deleted tracks"
+)
+
+failed_get_track_count = prometheus_client.Counter(
+    "failed_get_track_count",
+    "Number of failed attempts to get track by id"
+)
 
 name = 'TrackCatalog with Recommendations Service'
 
 
 @track_catalog_router.get('/')
 def get_tracks(track_catalog_service: TrackCatalogService = Depends(TrackCatalogService)) -> list[Track]:
+    get_tracks_count.inc(1)
     return track_catalog_service.get_tracks()
 
 
@@ -25,8 +59,10 @@ def get_tracks(track_catalog_service: TrackCatalogService = Depends(TrackCatalog
 def get_track(track_id: UUID, track_catalog_service: TrackCatalogService = Depends(TrackCatalogService)) -> Track:
     track = track_catalog_service.get_track_by_id(track_id)
     if track:
+        get_track_count.inc(1)
         return track.dict()
     else:
+        failed_get_track_count.inc(1)
         raise HTTPException(404, f'Trackwith id={track_id} not found')
 
 
@@ -38,6 +74,7 @@ def add_track(request: CreateTrackRequest,
         author=request.author,
         genre=request.genre,
         description=request.description)
+    add_track_count.inc(1)
     return new_track.dict()
 
 
@@ -46,10 +83,19 @@ def update_track(track_id: UUID, request: CreateTrackRequest,
                  track_catalog_service: TrackCatalogService = Depends(TrackCatalogService)) -> Track:
     updated_track = track_catalog_service.update_track(track_id, request.name, request.author, request.genre,
                                                        request.description)
+    update_track_count.inc(1)
     return updated_track.dict()
 
 
 @track_catalog_router.delete('/delete/{track_id}')
 def delete_track(track_id: UUID, track_catalog_service: TrackCatalogService = Depends(TrackCatalogService)) -> None:
     track_catalog_service.delete_track(track_id)
+    delete_track_count.inc(1)
     return {'message': 'Track deleted successfully'}
+
+@metrics_router.get('/metrics')
+def get_metrics():
+    return Response(
+        media_type="text/plain",
+        content=prometheus_client.generate_latest()
+    )
--- track-library-service/app/main.py
+++ track-library-service/app/main.py
@@ -4,19 +4,28 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import asyncio
 from fastapi import FastAPI
-
+from logging_loki import LokiHandler
+import logging
 
 
 from app.settings import settings
 
-from app.endpoints.track_router import track_catalog_router
+from app.endpoints.track_router import track_catalog_router, metrics_router
 
 
 name='TrackCatalog with Recommendations Service'
 
 app = FastAPI(title=name)
 
+loki_logs_handler = LokiHandler(
+    url="http://loki:3100/loki/api/v1/push",
+    tags={"application": "fastapi"},
+    version="1",
+)
 
+
+logger = logging.getLogger("uvicorn.access")
+logger.addHandler(loki_logs_handler)
 
 
 # @app.on_event('startup')
@@ -26,4 +35,4 @@ app = FastAPI(title=name)
 
 
 app.include_router(track_catalog_router, prefix='/track-api')
-
+app.include_router(metrics_router)
